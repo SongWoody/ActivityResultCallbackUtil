c9783e7bf8315004ffc9d3c47f416a50070c35e9
SongWoody <syg1138@gmail.com>
2020-12-23 13:32:49 +0900

    코드 정리 및 주석 추가

--- app/src/main/java/com/woody/activityresultcallbackutil/MainActivity.kt
+++ app/src/main/java/com/woody/activityresultcallbackutil/MainActivity.kt
@@ -2,9 +2,7 @@ package com.woody.activityresultcallbackutil
 
 import android.app.Activity
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import androidx.databinding.DataBindingUtil
 import com.woody.activityresultcallbackutil.core.BaseActivity
 import com.woody.activityresultcallbackutil.databinding.ActivityMainBinding
@@ -16,9 +14,14 @@ class MainActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
+        // button click
         binding.requestButton.setOnClickListener {
+            // #1. observer 등록 및 intent 실행
             val intent = Intent(this, Main2Activity::class.java)
             activityResultHelper.addActResultObserver(intent, Main2Activity.REQUEST_CODE) { resultCode, data ->
+                // #2 activity 결과 확인
+                // BaseActivity 의 onActivityResult 에서 받은 결과를
+                // ActivityResultHelper 를 통해 여기서 받게 됨
                 if (resultCode == Activity.RESULT_OK && data != null) {
                     val resultText = data.getStringExtra(Main2Activity.KEY_RESULT_TEXT)
                     binding.resultText.text = resultText
